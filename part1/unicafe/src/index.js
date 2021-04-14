0808e9d73b71308b8832e99c078ecd0ffac75292
skyfall947 <atencio947@gmail.com>
2021-04-14 18:12:25 -0400

    1.7: unicafe, step 2 - solved

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 
 
 const Heading = ({title}) => <h2>{title}</h2>;
-const Button = ({handleClick, content}) => <button onClick={handleClick}>{content}</button>
-const Display = ({title, votes}) => <span>{title}: <b>{votes} </b></span>
+
+const Button = ({handleClick, content}) => {
+  return(
+    <button onClick={handleClick}>{content}</button>
+  )
+};
+
+const Display = ({title, value}) => <span>{title}: <b>{value?value:0} </b></span>;
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
   const [neutral, setNeutral] = useState(0);
-
+  const total = good + neutral + bad;
+  
   return(
     <>
       <Heading title='Give some FEEDBACK!'/>
@@ -19,9 +26,13 @@ const App = () => {
       <Button content='Bad' handleClick={()=> setBad(bad+1)}/>
       <Heading title='Statistics'/>
       <div>
-        <Display title='Good' votes={good}/>
-        <Display title='Neutral' votes={neutral}/>
-        <Display title='Bad' votes={bad}/>
+        <Display title='Good' value={good}/>
+        <Display title='Neutral' value={neutral}/>
+        <Display title='Bad' value={bad}/>
+        <br/>
+        <Display title='All' value={total} />
+        <Display title='Average' value={good-bad/total}/>
+        <Display title='Positive' value={good/total} />
       </div>
     </>
   )
